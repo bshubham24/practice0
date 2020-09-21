fd49ffd0875ec7a3f33380b4fd457522c33a1163
bshubham24 <71620973+bshubham24@users.noreply.github.com>
2020-09-21 14:27:11 +0530

    Update Hello.java

--- src/com/cagpi/Hello.java
+++ src/com/cagpi/Hello.java
@@ -3,6 +3,7 @@ package com.cagpi;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World123");
+		System.out.println("New line");
 	}
 }
